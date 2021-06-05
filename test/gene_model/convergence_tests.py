f22f98eb490b12c9be3b1d2bb0b7cfa13c0d0c9f
tesseradecades <nwe8978@rit.edu>
2021-06-05 15:07:46 -0400

    formatting

--- test/gene_model/convergence_tests.py
+++ test/gene_model/convergence_tests.py
@@ -2,6 +2,7 @@ from typing import List
 
 from gene_model.convergence_tests import hamming_distance
 
+
 class Test_hamming_distance:
     @staticmethod
     def test_empty_lists():
@@ -10,11 +11,11 @@ class Test_hamming_distance:
         list2: List[int] = []
 
         # act
-        actual:int = hamming_distance(list1,list2)
+        actual: int = hamming_distance(list1, list2)
 
         # assert
         assert actual == 0
-    
+
     @staticmethod
     def test_one_empty_list():
         # arrange
@@ -22,42 +23,42 @@ class Test_hamming_distance:
         nonempty_list: List[int] = [0]
 
         # act
-        actual:int = hamming_distance(empty_list,nonempty_list)
+        actual: int = hamming_distance(empty_list, nonempty_list)
 
         # assert
         assert actual == len(nonempty_list)
-    
+
     @staticmethod
     def test_equal_lists():
         # arrange
         some_list: List[int] = [0]
 
         # act
-        actual:int = hamming_distance(some_list,some_list)
+        actual: int = hamming_distance(some_list, some_list)
 
         # assert
         assert actual == 0
-    
+
     @staticmethod
     def test_different_lists():
         # arrange
-        one_list: List[int]=[1]
+        one_list: List[int] = [1]
         zero_list: List[int] = [0]
 
         # act
-        actual:int = hamming_distance(one_list,zero_list)
+        actual: int = hamming_distance(one_list, zero_list)
 
         # assert
         assert actual == 1
-    
+
     @staticmethod
     def test_different_lists_of_different_length():
         # arrange
-        one_list: List[int]=[1,0]
+        one_list: List[int] = [1, 0]
         zero_list: List[int] = [0]
 
         # act
-        actual:int = hamming_distance(one_list,zero_list)
+        actual: int = hamming_distance(one_list, zero_list)
 
         # assert
         assert actual == 2
