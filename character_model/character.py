ec464e1cabec6564c9c35c6b3f20c9b98aef8090
tesseradecades <nwe8978@rit.edu>
2021-06-05 11:05:15 -0400

    individual abstract class

--- character_model/character.py
+++ character_model/character.py
@@ -1,40 +1,36 @@
+from typing import Dict, List
+
 from character_model.actions.attacks import UnarmedStrike
-from gene_model.chromosome import Chromosome
+from gene_model.chromosome import Chromosome, Individual
 
 
-class Character:
-    def __init__(self, chromosomes: list):
+class Character(Individual):
+    def __init__(self, chromosomes: List[Chromosome]):
         """
         Chromosomes
         [
             0:26 -> ability score chromosomes, each 6 bits in length
         ]
         """
+        super().__init__(chromosomes)
         self.__charisma_score = 8
         self.__constitution_score = 8
         self.__dexterity_score = 8
         self.__intelligence_score = 8
         self.__strength_score = 8
         self.__wisdom_score = 8
-        self.__chromosomes: list = chromosomes
         self.__assign_ability_scores()
 
     def __assign_ability_scores(self):
-        points_applied: dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
-        for chromosome in self.__chromosomes[:27]:
+        points_applied: Dict[int, int] = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+        for chromosome in self._chromosomes[:27]:
             points_applied[sum(chromosome.get_genes())] += 1
-        print(points_applied)
         self.__strength_score += self.__render_added_score(points_applied[1])
         self.__dexterity_score += self.__render_added_score(points_applied[2])
         self.__constitution_score += self.__render_added_score(points_applied[3])
         self.__intelligence_score += self.__render_added_score(points_applied[4])
         self.__wisdom_score += self.__render_added_score(points_applied[5])
         self.__charisma_score += self.__render_added_score(points_applied[6])
-
-    def __get_genes(self):
-        all_genes: list = []
-        for chromosome in self.__chromosomes:
-            all_genes += chromosome.get_genes()
 
     @staticmethod
     def __get_modifier(ability_score: int) -> int:
@@ -54,7 +50,7 @@ class Character:
         return f"Strength:\t{self.__strength_score}\nDexterity:\t{self.__dexterity_score}\nConstitution:\t{self.__constitution_score}\nIntelligence:\t{self.__intelligence_score}\nWisdom:\t{self.__wisdom_score}\nCharisma:\t{self.__charisma_score}"
 
     def __repr__(self):
-        return f"{self.__str__()}\nGenotype:\t{self.__get_genes()}"
+        return f"{self.__str__()}\n{super().__repr__()}"
 
     def get_actions(self) -> list:
         return [UnarmedStrike(self.__get_modifier(self.__strength_score))]
--- gene_model/chromosome.py
+++ gene_model/chromosome.py
@@ -1,9 +1,13 @@
+from abc import ABC
+from typing import List
+
+
 class Chromosome:
-    def __init__(self, genes: list = [], length: int = 0):
+    def __init__(self, genes: List[int] = [], length: int = 0):
         if genes:
-            self.__genes: list = genes
+            self.__genes: List[int] = genes
         else:
-            self.__genes: list = [0] * length
+            self.__genes: List[int] = [0] * length
 
     def __repr__(self):
         return self.get_genes()
@@ -15,5 +19,18 @@ class Chromosome:
         else:
             self.__genes[index] = 1
 
-    def get_genes(self):
+    def get_genes(self) -> List[int]:
         return list(self.__genes)
+
+
+class Individual(ABC):
+    def __init__(self, chromosomes: List[Chromosome]):
+        self._chromosomes: List[Chromosome] = chromosomes
+
+    def __repr__(self):
+        return f"Genotype:\t{self._get_genes()}"
+
+    def _get_genes(self) -> List[int]:
+        all_genes: List[int] = []
+        for chromosome in self._chromosomes:
+            all_genes += chromosome.get_genes()
