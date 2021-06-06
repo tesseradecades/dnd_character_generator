21e8254ec8aee18d8b3bdb0fc5f326d8c2ab7a89
tesseradecades <nwe8978@rit.edu>
2021-06-05 21:48:57 -0400

    endpoint

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from typing import List
+
 from fastapi import FastAPI
 
+from character_model.character import Character
+from character_model.simulation import DndSimulation
+
 app = FastAPI()
+
+
+@app.get("/getCharacters")
+async def get_characters():
+    characters: List[Character] = DndSimulation().get_characters()
+    return [repr(character) for character in characters]
 
 
 @app.get("/health")
